1d258ebd5c6b744be32d2c0357aa4d78a7940678
sdia <seydou.dia@olx.com>
2017-06-01 16:12:58 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,4 @@
+https://www.blog.pythonlibrary.org/2016/03/01/python-101-all-about-imports/
+
+
+check the section on Relative import.
---
+++ subpackage1/module_x.py
@@ -0,0 +1,12 @@
+from .module_y import spam as ham
+
+
+
+def main():
+    ham()
+
+
+
+if __name__ == '__main__':
+    # this won't work
+    main()
